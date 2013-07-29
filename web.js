bdfa3bff11c1e1b3b3f2719604d31f6e63e96097
Ion Berges <ionberges@gmail.com>
2013-07-29 23:59:56 +0000

    change web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
+
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var buff = new Buffer(fs.readFileSync("index.html"));
+var app = express.createServer(express.logger());
+var content = buff.toString();
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
+app.get('/', function(request, response) {  
+response.send(content);});
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function() {  console.log("Listening on " + port);});
+
+
+
