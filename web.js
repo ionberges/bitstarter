9ef6e1b6cf0a93d1fddaa0f9c1af862abdef53f9
Ubuntu <ubuntu@ip-10-204-49-169.ec2.internal>
2013-08-05 16:37:20 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 
-var express = require('express');
+var express = require("express");
+  var app = express();
 var fs = require('fs');
 var buff = new Buffer(fs.readFileSync("index.html"));
-var app = express.createServer(express.logger());
 var content = buff.toString();
 
 app.get('/', function(request, response) {  
 response.send(content);});
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {  console.log("Listening on " + port);});
 
 
